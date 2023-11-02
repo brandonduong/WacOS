803c800737cd8236208be6508cd09c7773f78aac
Brandon Duong <brandon.duong@outlook.com>
2023-11-02 10:17:43 -0400

    finalized name

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Skill Issue",
+  title: "WacOS",
   description: "Just get a job.",
 };
 
